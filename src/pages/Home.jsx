6049b82f804adccf1ce5461a60414b31c39f57ec
Farid Amimri <farid.amimri@gmail.com>
2022-12-20 12:09:49 +0100

    Fetch user stats & use it for chart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,60 @@
 /** @format */
 
 import styled from 'styled-components'
-import { userData } from '../data'
 import Features from '../components/Features'
 import Chart from '../components/Chart'
 import Members from '../components/Members'
 import Transactions from '../components/Transactions'
+import { useState, useMemo, useEffect } from 'react'
+import { userRequest } from '../requests'
 
 function Home() {
+  const [userStats, setUserStats] = useState([])
+
+  const MONTHS = useMemo(
+    () => [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Agu',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+    ],
+    []
+  )
+
+  useEffect(() => {
+    const getUserStats = async () => {
+      try {
+        const res = await userRequest.get('users/statistics')
+        console.log(res.data)
+        res.data.map((item) =>
+          setUserStats((prev) => [
+            // take prev item, spread it and add new data
+            ...prev,
+            { name: MONTHS[item._id - 1], 'Active User': item.total }
+          ])
+        )
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getUserStats()
+  }, [MONTHS])
+
   return (
     <Container>
       <Features />
       <ChartWrapper>
         <Chart
           className='chart'
-          data={userData}
+          data={userStats}
           title='User Analytics'
           dataKey='Active User'
           grid
