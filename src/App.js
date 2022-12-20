fe73602a1cfa7ae139a15bc4fc14a68f14d193dd
Farid Amimri <farid.amimri@gmail.com>
2022-12-20 10:29:53 +0100

    Add Login Route & admin verifiaction to access pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 /** @format */
 
 import styled from 'styled-components'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import Home from './pages/Home'
@@ -11,24 +16,36 @@ import NewUser from './pages/NewUser'
 import Products from './pages/Products'
 import Product from './pages/Product'
 import NewProduct from './pages/NewProduct'
+import Login from './pages/Login'
 
 function App() {
+  const admin = JSON.parse(
+    JSON.parse(localStorage.getItem('persist:root')).user
+  ).currentUser.isAdmin
+  // If Admin is true, render pages
+
   return (
     <div className='App'>
       <Router>
         <Navbar />
-        <Pages>
-          <Sidebar />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/users' element={<Users />} />
-            <Route path='/user/:userId' element={<User />} />
-            <Route path='/newUser' element={<NewUser />} />
-            <Route path='/products' element={<Products />} />
-            <Route path='/product/:productId' element={<Product />} />
-            <Route path='/newProduct' element={<NewProduct />} />
-          </Routes>
-        </Pages>
+        {admin && (
+          <Pages>
+            <Sidebar />
+            <Routes>
+              <Route
+                path='/login'
+                element={admin ? <Navigate to='/' /> : <Login />}
+              />
+              <Route path='/' element={<Home />} />
+              <Route path='/users' element={<Users />} />
+              <Route path='/user/:userId' element={<User />} />
+              <Route path='/newUser' element={<NewUser />} />
+              <Route path='/products' element={<Products />} />
+              <Route path='/product/:productId' element={<Product />} />
+              <Route path='/newProduct' element={<NewProduct />} />
+            </Routes>
+          </Pages>
+        )}
       </Router>
     </div>
   )
