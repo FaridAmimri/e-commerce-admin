874ff4aac2972ac9d4b3107aaa339badda223a03
Farid Amimri <farid.amimri@gmail.com>
2022-12-21 11:09:55 +0100

    Set getPoducts fn & deleteProduct fn with productSlice

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,7 +1,15 @@
 /** @format */
 
 import { loginStart, loginFailure, loginSuccess } from './userSlice'
-import { publicRequest } from '../requests'
+import { publicRequest, userRequest } from '../requests'
+import {
+  getProductStart,
+  getProductSuccess,
+  getProductFailure,
+  deleteProductStart,
+  deleteProductSuccess,
+  deleteProductFailure
+} from './productSlice'
 
 export const login = async (dispatch, user) => {
   // Take dispatch and user info
@@ -11,5 +19,27 @@ export const login = async (dispatch, user) => {
     dispatch(loginSuccess(res.data)) // payload here will be res.data which is user info
   } catch (error) {
     dispatch(loginFailure())
+  }
+}
+
+export const getProducts = async (dispatch) => {
+  // Take dispatch only
+  dispatch(getProductStart())
+  try {
+    const res = await publicRequest.get('/products') // Take products data
+    dispatch(getProductSuccess(res.data)) // payload here will be res.data which is products data
+  } catch (error) {
+    dispatch(getProductFailure())
+  }
+}
+
+export const deleteProduct = async (id, dispatch) => {
+  // Take dispatch only
+  dispatch(deleteProductStart())
+  try {
+    // const res = await userRequest.delete(`/products/delete/${id}`) // Take products data
+    dispatch(deleteProductSuccess(id)) // payload here will be res.data which is products data
+  } catch (error) {
+    dispatch(deleteProductFailure())
   }
 }
